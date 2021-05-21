eb572c4be90f4565f47dc8785bf916d18e305af9
Камчыбек Абдыназаров <tilekberdiev@outlook.com>
2021-05-21 18:56:54 +0600

    create Task8.3

---
+++ 8.3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+int NumF(int num, int i,int res)
+{
+    res *= i;
+    i++;
+    if (num < i)
+        return res;
+    else
+        return NumF(num,i,res);
+}
+
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(NumF(num, 1,1));
+
