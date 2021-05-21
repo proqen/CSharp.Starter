d95926a5ecd528fa8589453786c02828a50cef8d
Камчыбек Абдыназаров <tilekberdiev@outlook.com>
2021-05-21 16:41:41 +0600

    create Task.5.2

---
+++ Task5.2/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+int num = int.Parse(Console.ReadLine());
+
+if (Convert.ToString(num, 2)[^1] == '0' && num % 2 == 0)
+    Console.WriteLine("YES"); //чет
+else
+    Console.WriteLine("NO");
