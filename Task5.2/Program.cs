2a2b5364b3b30c10ca97a8edb6193e21e6b1b4c4
Камчыбек Абдыназаров <tilekberdiev@outlook.com>
2021-05-23 04:08:03 +0600

    added new algorithm

--- Task5.2/Program.cs
+++ Task5.2/Program.cs
@@ -1,8 +1,19 @@
 ﻿using System;
 
+bool CheckNum(string num) =>
+    num[^1] switch
+    {
+        '1' => false,
+        '3' => false,
+        '5' => false,
+        '7' => false,
+        '9' => false,
+        _ => true
+    };
+
 var res = "нечетный";
 int num = int.Parse(Console.ReadLine());
-if (Convert.ToString(num, 2)[^1] == '0' && num % 2 == 0)
+if (num % 2 == 0 && Convert.ToString(num, 2)[^1] == '0' &&  (num & 1) == 0 && CheckNum(num.ToString()))
     res = "четный";
 
 Console.WriteLine(res);
